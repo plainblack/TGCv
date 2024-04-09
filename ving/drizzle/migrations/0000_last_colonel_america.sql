abb068f806ebc73b888c3655c4a01c6f4f293081
colin <colink@perldreamer.com>
2024-04-09 09:12:05 -0700

    Reset due to changes in core database schema (foreign keys)

---
+++ ving/drizzle/migrations/0000_last_colonel_america.sql
@@ -0,0 +1,139 @@
+CREATE TABLE `apikeys` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`name` varchar(60) NOT NULL DEFAULT '',
+	`url` text NOT NULL,
+	`reason` text NOT NULL,
+	`privateKey` varchar(39) NOT NULL DEFAULT '',
+	`userId` varchar(36) NOT NULL,
+	CONSTRAINT `apikeys_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenancefiles` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`s3FileId` varchar(36) NOT NULL,
+	`maintenanceTicketId` varchar(36) NOT NULL,
+	CONSTRAINT `maintenancefiles_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenanceitems` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`name` varchar(60) NOT NULL DEFAULT '',
+	`status` enum('in_use','retired') NOT NULL DEFAULT 'in_use',
+	`maintenanceItemSetId` varchar(36) NOT NULL,
+	CONSTRAINT `maintenanceitems_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenanceitemsets` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`name` varchar(60) NOT NULL DEFAULT '',
+	`status` enum('in_use','retired') NOT NULL DEFAULT 'in_use',
+	CONSTRAINT `maintenanceitemsets_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenanceremarks` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`description` mediumtext NOT NULL,
+	`resolution` enum('resolved','unresolved','n_a') NOT NULL DEFAULT 'n_a',
+	`resolutionMinutes` int NOT NULL DEFAULT 0,
+	`submittedBy` varchar(64) NOT NULL DEFAULT '',
+	`maintenanceTicketId` varchar(36) NOT NULL,
+	CONSTRAINT `maintenanceremarks_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenanceschedules` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`recurrence` enum('yearly','monthly','weekly','daily') NOT NULL DEFAULT 'monthly',
+	`months` int NOT NULL DEFAULT 0,
+	`weeks` int NOT NULL DEFAULT 0,
+	`days` int NOT NULL DEFAULT 0,
+	`maintenanceItemSetId` varchar(36) NOT NULL,
+	`maintenanceTaskId` varchar(36) NOT NULL,
+	CONSTRAINT `maintenanceschedules_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenancetasks` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`description` mediumtext NOT NULL,
+	`maintenanceItemSetId` varchar(36) NOT NULL,
+	CONSTRAINT `maintenancetasks_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `maintenancetickets` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`description` mediumtext NOT NULL,
+	`type` enum('routine','needs_help') NOT NULL DEFAULT 'routine',
+	`severity` enum('working','intermittent','down') NOT NULL DEFAULT 'working',
+	`status` enum('resolved','unresolved') NOT NULL DEFAULT 'resolved',
+	`resolutionMinutes` int NOT NULL DEFAULT 0,
+	`submittedBy` varchar(64) NOT NULL DEFAULT '',
+	`maintenanceTaskId` varchar(36) NOT NULL,
+	`maintenanceItemId` varchar(36) NOT NULL,
+	CONSTRAINT `maintenancetickets_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `s3files` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`filename` varchar(256) NOT NULL DEFAULT '',
+	`extension` varchar(10) NOT NULL DEFAULT '',
+	`contentType` varchar(256) NOT NULL DEFAULT '',
+	`s3folder` varchar(256) NOT NULL DEFAULT '',
+	`sizeInBytes` int NOT NULL DEFAULT 0,
+	`metadata` json NOT NULL DEFAULT ('{}'),
+	`status` enum('pending','ready','postProcessingFailed','verifyFailed') NOT NULL DEFAULT 'pending',
+	`icon` enum('pending','thumbnail','extension','self') NOT NULL DEFAULT 'pending',
+	`userId` varchar(36) NOT NULL,
+	CONSTRAINT `s3files_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` varchar(36) NOT NULL DEFAULT 'uuid-will-be-generated',
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`username` varchar(60) NOT NULL DEFAULT '',
+	`email` varchar(256) NOT NULL DEFAULT '',
+	`realName` varchar(60) NOT NULL DEFAULT '',
+	`password` varchar(256) NOT NULL DEFAULT 'no-password-specified',
+	`passwordType` enum('bcrypt') NOT NULL DEFAULT 'bcrypt',
+	`useAsDisplayName` enum('username','email','realName') NOT NULL DEFAULT 'username',
+	`verifiedEmail` boolean NOT NULL DEFAULT false,
+	`admin` boolean NOT NULL DEFAULT false,
+	`maintenanceManager` boolean NOT NULL DEFAULT false,
+	`productionManager` boolean NOT NULL DEFAULT false,
+	`developer` boolean NOT NULL DEFAULT false,
+	`avatarType` enum('robot','uploaded') NOT NULL DEFAULT 'robot',
+	`avatarId` varchar(36) DEFAULT null,
+	CONSTRAINT `users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `usernameIndex` UNIQUE(`username`),
+	CONSTRAINT `emailIndex` UNIQUE(`email`)
+);
+--> statement-breakpoint
+ALTER TABLE `apikeys` ADD CONSTRAINT `apikeys_user_90ada4_fk` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenancefiles` ADD CONSTRAINT `maintenancefiles_s3file_bc83f09_fk` FOREIGN KEY (`s3FileId`) REFERENCES `s3files`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenancefiles` ADD CONSTRAINT `maintenancefiles_ticket_71bc799_fk` FOREIGN KEY (`maintenanceTicketId`) REFERENCES `maintenancetickets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenanceitems` ADD CONSTRAINT `maintenanceitems_itemSet_5f29c163_fk` FOREIGN KEY (`maintenanceItemSetId`) REFERENCES `maintenanceitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenanceremarks` ADD CONSTRAINT `maintenanceremarks_ticket_7bde9655_fk` FOREIGN KEY (`maintenanceTicketId`) REFERENCES `maintenancetickets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenanceschedules` ADD CONSTRAINT `maintenanceschedules_maintenanceItemSet_2bb27d4e_fk` FOREIGN KEY (`maintenanceItemSetId`) REFERENCES `maintenanceitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenanceschedules` ADD CONSTRAINT `maintenanceschedules_maintenanceTask_48169202_fk` FOREIGN KEY (`maintenanceTaskId`) REFERENCES `maintenancetasks`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenancetasks` ADD CONSTRAINT `maintenancetasks_itemSet_75c8db95_fk` FOREIGN KEY (`maintenanceItemSetId`) REFERENCES `maintenanceitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenancetickets` ADD CONSTRAINT `maintenancetickets_task_1206e190_fk` FOREIGN KEY (`maintenanceTaskId`) REFERENCES `maintenancetasks`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `maintenancetickets` ADD CONSTRAINT `maintenancetickets_item_120b9c02_fk` FOREIGN KEY (`maintenanceItemId`) REFERENCES `maintenanceitems`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `s3files` ADD CONSTRAINT `s3files_user_40cb3d4d_fk` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `users` ADD CONSTRAINT `users_avatar_39d62890_fk` FOREIGN KEY (`avatarId`) REFERENCES `s3files`(`id`) ON DELETE set null ON UPDATE no action;
