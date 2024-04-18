2432e4912b9afc5dcd7dc0f11df66be2d5099471
colin <colink@perldreamer.com>
2024-04-18 15:51:26 -0700

    On ticket editing for remarks, add in file interface, currently broken due to missing AWS config

--- pages/maintenanceticket/[id]/index.vue
+++ pages/maintenanceticket/[id]/index.vue
@@ -32,6 +32,27 @@
 
 
     </div>
+    <div class="mt-5 surface-card p-5 border-1 surface-border border-round">
+        <h2 class="mt-0">Comments</h2>
+        <div class="mt-5 surface-card p-5 border-1 surface-border border-round" v-for="remark in maintenanceremarks.records">
+            <h3 class="mt-0">{{ remark.props.submittedBy }} at {{ dt.formatDateTime(remark.props.updatedAt) }}</h3>
+            <div v-if="! remark.props.editing">{{ remark.props.description }}</div>
+            <div v-else>
+                <div>
+                    <FormInput name="description" type="text" v-model="remark.props.description" required
+                    label="Description"/>
+                </div>
+                <div>
+                    <Button class="w-auto" severity="success" title="Save" alt="Save" @click="remark.update(); remark.props.editing=false;">
+                        <i class="pi pi-plus mr-1"></i> Save
+                    </Button>
+            </div>
+            </div>
+            <div> 
+                <Button icon="pi pi-pencil" severity="success" title="Edit" alt="Edit Comment" @click="remark.props.editing = true" />
+                <Button v-if="remark.meta?.isOwner"  title="Delete" alt="Delete Comment" icon="pi pi-trash" severity="danger" @click="remark.delete()" /></div>
+        </div>
+    </div>
     <div class="surface-card p-4 border-1 surface-border border-round">
 
         <InputGroup>
@@ -65,9 +86,6 @@
             </Column>
             <Column header="Manage">
                 <template #body="slotProps">
-                    <NuxtLink :to="`/maintenanceremark/${slotProps.data.props.id}`" class="mr-2 no-underline">
-                        <Button icon="pi pi-eye"  title="View" alt="View Maintenance Remark" />
-                    </NuxtLink>
                     <NuxtLink v-if="slotProps.data.meta?.isOwner" :to="`/maintenanceremark/${slotProps.data.props.id}/edit`" class="mr-2 no-underline">
                         <Button icon="pi pi-pencil" severity="success" title="Edit" alt="Edit Maintenance Remark" />
                     </NuxtLink>
@@ -106,6 +124,50 @@
             </div>
         </Form>
     </div>
+    <div class="surface-card p-4 border-1 surface-border border-round">
+
+<DataTable :value="maintenancefiles.records" stripedRows @sort="(e) => maintenancefiles.sortDataTable(e)">
+    
+    <Column field="props.createdAt" header="Created At" sortable>
+        <template #body="slotProps">
+            {{ dt.formatDateTime(slotProps.data.props.createdAt) }}
+        </template>
+    </Column>
+    <Column field="props.updatedAt" header="Updated At" sortable>
+        <template #body="slotProps">
+            {{ dt.formatDateTime(slotProps.data.props.updatedAt) }}
+        </template>
+    </Column>
+    <Column field="props.s3FileId" header="File" sortable>
+        <template #body="slotProps">
+            <img :src="slotProps.data.related?.s3file?.meta?.thumbnailUrl" />
+        </template>
+    </Column>
+    <Column header="Manage">
+        <template #body="slotProps">
+            <NuxtLink :to="`/maintenancefile/${slotProps.data.props.id}`" class="mr-2 no-underline">
+                <Button icon="pi pi-eye"  title="View" alt="View Maintenance File" />
+            </NuxtLink>
+            <NuxtLink v-if="slotProps.data.meta?.isOwner" :to="`/maintenancefile/${slotProps.data.props.id}/edit`" class="mr-2 no-underline">
+                <Button icon="pi pi-pencil" severity="success" title="Edit" alt="Edit Maintenance File" />
+            </NuxtLink>
+            <Button v-if="slotProps.data.meta?.isOwner"  title="Delete" alt="Delete Maintenance File" icon="pi pi-trash" severity="danger" @click="slotProps.data.delete()" />
+        </template>
+    </Column>
+</DataTable>
+<Pager :kind="maintenancefiles" />
+</div>
+    <div class="mt-5 surface-card p-5 border-1 surface-border border-round">
+        <h2 class="mt-0">Add Files</h2>
+        <div class="mb-4">
+            <client-only>
+                <Dropzone
+                    :acceptedFiles="['.png', '.jpeg', '.jpg', '.gif', '.pdf', '.tiff', '.svg', '.dxf', '.csv']"
+                    :afterUpload="uploadMaintenanceFile">
+                </Dropzone>
+            </client-only>
+        </div>
+    </div>
 
     <div class="mt-3" v-if="maintenanceticket.meta?.isOwner">
         <NuxtLink :to="`/maintenanceticket/${maintenanceticket.props?.id}/edit`" class="no-underline mr-2 mb-2">
@@ -140,14 +202,30 @@ const maintenanceremarks = useVingKind({
         await maintenanceticket.fetch();
     }
 });
+const maintenancefiles = useVingKind({
+    listApi: `/api/${restVersion()}/maintenancefile`,
+    createApi: `/api/${restVersion()}/maintenancefile`,
+    query: { includeMeta: true, sortBy: 'createdAt', sortOrder: 'desc', includeRelated: ['s3file'], },
+    newDefaults: { s3FileId: '', maintenanceTicketId: maintenanceticket.props.id },
+});
+
+const uploadMaintenanceFile = async (s3file) => {
+    const response = await maintenancefiles.create();
+    const file = maintenancefiles.find(response.data?.props?.id);
+    file.importS3File('s3file', s3file.props?.id);
+};
+
 await Promise.all([
     maintenanceremarks.search(),
     maintenanceremarks.fetchPropsOptions(),
+    maintenancefiles.search(),
+    maintenancefiles.fetchPropsOptions(),
     maintenanceticket.fetch(),
 ]);
 onBeforeRouteLeave(() => {
     maintenanceremarks.dispose();
     maintenanceticket.dispose();
+    maintenancefiles.dispose();
 });
 const dt = useDateTime();
 const breadcrumbs = [
