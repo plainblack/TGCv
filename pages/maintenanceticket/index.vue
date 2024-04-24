b5a0b46c9af94107c5140c23d5fe7e27e63eaecd
colin <colink@perldreamer.com>
2024-04-24 16:12:38 -0700

    Add description snippet to tickets table.  Fix syntax error.

--- pages/maintenanceticket/index.vue
+++ pages/maintenanceticket/index.vue
@@ -60,63 +60,68 @@
                     </NuxtLink>
                 </template>
             </Column>
-            <Column field="props.type" header="Type" sortable v-if="maintenancetickets.query.type == ''"">
+            <Column field="props.type" header="Type" sortable v-if="maintenancetickets.query.type == ''">
                 <template #body="slotProps">
-                {{ enum2label(slotProps.data.props.type, maintenancetickets.propsOptions.type) }}
-</template>
-</Column>
-<Column field="props.maintenanceItemId" header="Maintenance Item" sortable>
-    <template #body="slotProps">
-        {{ allmaintenanceitems.find(slotProps.data.props?.maintenanceItemId)?.props?.name }}
-    </template>
-</Column>
-<Column field="props.maintenanceTaskId" header="Task" sortable>
-    <template #body="slotProps">
-        {{ allmaintenancetasks.find(slotProps.data.props?.maintenanceTaskId)?.props?.description }}
-    </template>
-</Column>
-<Column field="props.updatedAt" header="Updated At" sortable>
-    <template #body="slotProps">
-        {{ dt.formatDate(slotProps.data.props.updatedAt) }}
-    </template>
-</Column>
-</DataTable>
-<Pager :kind="maintenancetickets" />
-</div>
-<div class="mt-5 surface-card p-5 border-1 surface-border border-round">
-    <h2 class="mt-0">Create Maintenance Ticket</h2>
+                    {{ enum2label(slotProps.data.props.type, maintenancetickets.propsOptions.type) }}
+                </template>
+            </Column>
+            <Column field="props.description" header="Description">
+                <template #body="slotProps">
+                    {{ slotProps.data.props.description.slice(0, 60) }}
+                </template>
+            </Column>
+            <Column field="props.maintenanceItemId" header="Maintenance Item" sortable>
+                <template #body="slotProps">
+                    {{ allmaintenanceitems.find(slotProps.data.props?.maintenanceItemId)?.props?.name }}
+                </template>
+            </Column>
+            <Column field="props.maintenanceTaskId" header="Task" sortable>
+                <template #body="slotProps">
+                    {{ allmaintenancetasks.find(slotProps.data.props?.maintenanceTaskId)?.props?.description }}
+                </template>
+            </Column>
+            <Column field="props.updatedAt" header="Updated At" sortable>
+                <template #body="slotProps">
+                    {{ dt.formatDate(slotProps.data.props.updatedAt) }}
+                </template>
+            </Column>
+        </DataTable>
+        <Pager :kind="maintenancetickets" />
+    </div>
+    <div class="mt-5 surface-card p-5 border-1 surface-border border-round">
+        <h2 class="mt-0">Create Maintenance Ticket</h2>
 
-    <Form :send="() => maintenancetickets.create()">
-        <div class="flex gap-5 flex-column-reverse md:flex-row">
-            <div class="flex-auto p-fluid">
-                <MaintenanceItemTaskSelector :target="maintenancetickets" :maintenanceitems="allmaintenanceitems" />
+        <Form :send="() => maintenancetickets.create()">
+            <div class="flex gap-5 flex-column-reverse md:flex-row">
+                <div class="flex-auto p-fluid">
+                    <MaintenanceItemTaskSelector :target="maintenancetickets" :maintenanceitems="allmaintenanceitems" />
 
-                <div class="mb-4">
-                    <FormInput name="description" type="markdown" v-model="maintenancetickets.new.description" required
-                        label="Description" />
+                    <div class="mb-4">
+                        <FormInput name="description" type="markdown" v-model="maintenancetickets.new.description"
+                            required label="Description" />
+                    </div>
+                    <div class="mb-4">
+                        <FormInput type="select" name="type" :options="maintenancetickets.propsOptions?.type"
+                            v-model="maintenancetickets.new.type" label="Type" />
+                    </div>
+                    <div class="mb-4">
+                        <FormInput type="select" name="severity" :options="maintenancetickets.propsOptions?.severity"
+                            v-model="maintenancetickets.new.severity" label="Severity" />
+                    </div>
+                    <div class="mb-4">
+                        <FormInput name="submittedBy" type="text" v-model="maintenancetickets.new.submittedBy" required
+                            label="Submitted By" />
+                    </div>
+                    <div>
+                        <Button type="submit" class="w-auto" severity="success">
+                            <i class="pi pi-plus mr-1"></i> Create Maintenance Ticket
+                        </Button>
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <FormInput type="select" name="type" :options="maintenancetickets.propsOptions?.type"
-                        v-model="maintenancetickets.new.type" label="Type" />
-                </div>
-                <div class="mb-4">
-                    <FormInput type="select" name="severity" :options="maintenancetickets.propsOptions?.severity"
-                        v-model="maintenancetickets.new.severity" label="Severity" />
-                </div>
-                <div class="mb-4">
-                    <FormInput name="submittedBy" type="text" v-model="maintenancetickets.new.submittedBy" required
-                        label="Submitted By" />
-                </div>
-                <div>
-                    <Button type="submit" class="w-auto" severity="success">
-                        <i class="pi pi-plus mr-1"></i> Create Maintenance Ticket
-                    </Button>
-                </div>
+
             </div>
-
-        </div>
-    </Form>
-</div>
+        </Form>
+    </div>
 </template>
 
 <script setup>
