022e763eb3fd24be7013f81d79878685faeb9959
colin <colink@perldreamer.com>
2024-05-31 10:49:31 -0700

    Regenerate MaintenanceFile API endpoints

--- server/api/v1/maintenancefile/[id]/import-s3file.put.mjs
+++ server/api/v1/maintenancefile/[id]/import-s3file.put.mjs
@@ -6,23 +6,20 @@ export default defineEventHandler(async (event) => {
     const maintenancefiles = await useKind('MaintenanceFile');
     const { id } = getRouterParams(event);
     const maintenancefile = await maintenancefiles.findOrDie(id);
-    //const oldFile = await maintenancefile.parent('s3file');
+    let oldFile = undefined;
+    if (maintenancefile.get('s3FileId'))
+        oldFile = await maintenancefile.parent('s3file');
     const session = obtainSession(event);
     await maintenancefile.canEdit(session);
     const body = await getBody(event);
     const s3files = await useKind('S3File');
     const s3file = await s3files.findOrDie(body.s3FileId);
-    console.log("Going to postprocess S3File");
     await s3file.postProcessFile();
-    console.log("S3File post processed");
-    await s3file.verifyExtension(['png', 'jpeg', 'jpg', 'gif', 'pdf', 'tiff', 'svg', 'dxf', 'csv']);
-    console.log("S3File verified extension");
+    await s3file.verifyExtension(maintenancefile.parentPropSchema('s3file').relation.acceptedFileExtensions);
+    await s3file.verifyExactDimensions(300, 300);
     maintenancefile.set('s3FileId', s3file.get('id'));
     await maintenancefile.update();
-    console.log("Set sefileId on maintenanceFile");
-
-    //await oldFile.delete();
-    const description = await maintenancefile.describe(describeParams(event, session));
-    console.log("Described maintenance file");
-    return description;
+    if (oldFile)
+        await oldFile.delete();
+    return await maintenancefile.describe(describeParams(event, session));
 });
--- server/api/v1/maintenancefile/index.post.mjs
+++ server/api/v1/maintenancefile/index.post.mjs
@@ -1,6 +1,6 @@
 import { useKind } from '#ving/record/utils.mjs';
 import { describeParams, getBody, obtainSessionIfRole } from '#ving/utils/rest.mjs';
-import { defineEventHandler } from 'h3';
+import {defineEventHandler} from 'h3';
 export default defineEventHandler(async (event) => {
     const maintenancefiles = await useKind('MaintenanceFile');
     const session = obtainSessionIfRole(event, 'verifiedEmail');
