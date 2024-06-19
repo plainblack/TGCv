453c747e6245d438ea50bfdf17017be6aede284d
colin <colink@perldreamer.com>
2024-06-19 14:16:39 -0700

    First cut at script and data for importing maintenance items from TGC.

---
+++ import_tgc_hardware.mjs
@@ -0,0 +1,43 @@
+#!/usr/bin/env -S node --env-file=.env
+import { useKind } from '#ving/record/utils.mjs';
+import { maintenanceItems } from './maintenanceItems.mjs';
+
+
+const Sets = await useKind('HardwareItemSet');
+const Items = await useKind('HardwareItem');
+const Tasks = await useKind('HardwareTask');
+
+await Sets.delete();
+await Items.delete();
+await Tasks.delete();
+
+const seenSets = [];
+const itemPromises = [];
+
+for (const item of maintenanceItems) {
+    //Set == station
+    if (!seenSets[item.station]) {
+        //Create the Set
+        const set = await Sets.createAndVerify({
+            name: item.station,
+            status: item.status == 'In Use' ? 'in_use' : 'retired',
+        });
+        seenSets[item.station] = set;
+        //Create tasks for the Set
+        for (const task of item.tasks) {
+            await Tasks.createAndVerify({
+                description: task,
+                hardwareItemSetId: set.id,
+            });
+        }
+    }
+    //Create the item
+    itemPromises.push(Items.createAndVerify({
+        name: item.item,
+        status: item.status,
+        hardwareItemSetId: seenSets[item.station],
+    }));
+}
+
+Promise.all(itemPromises);
+console.log("Done");
