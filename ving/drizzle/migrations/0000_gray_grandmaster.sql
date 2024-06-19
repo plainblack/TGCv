d42317b76bb23326950fbbe809cbccadb9e36618
colin <colink@perldreamer.com>
2024-06-19 14:16:18 -0700

    Migration for modified naming schemes.

--- ving/drizzle/migrations/0000_gray_grandmaster.sql
+++ ving/drizzle/migrations/0000_gray_grandmaster.sql
@@ -10,26 +10,17 @@ CREATE TABLE `apikeys` (
 	CONSTRAINT `apikeys_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `maintenancefiles` (
-	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
-	`createdAt` timestamp NOT NULL DEFAULT (now()),
-	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-	`s3FileId` bigint unsigned NOT NULL,
-	`maintenanceTicketId` bigint unsigned NOT NULL,
-	CONSTRAINT `maintenancefiles_id` PRIMARY KEY(`id`)
-);
---> statement-breakpoint
-CREATE TABLE `maintenanceitems` (
+CREATE TABLE `hardwareitems` (
 	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
 	`createdAt` timestamp NOT NULL DEFAULT (now()),
 	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
 	`name` varchar(60) NOT NULL DEFAULT '',
 	`status` enum('in_use','retired') NOT NULL DEFAULT 'in_use',
-	`maintenanceItemSetId` bigint unsigned NOT NULL,
-	CONSTRAINT `maintenanceitems_id` PRIMARY KEY(`id`)
+	`hardwareItemSetId` bigint unsigned NOT NULL,
+	CONSTRAINT `hardwareitems_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `maintenanceitemsets` (
+CREATE TABLE `hardwareitemsets` (
 	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
 	`createdAt` timestamp NOT NULL DEFAULT (now()),
 	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
@@ -37,22 +28,10 @@ CREATE TABLE `maintenanceitemsets` (
 	`itemCount` int NOT NULL DEFAULT 0,
 	`taskCount` int NOT NULL DEFAULT 0,
 	`status` enum('in_use','retired') NOT NULL DEFAULT 'in_use',
-	CONSTRAINT `maintenanceitemsets_id` PRIMARY KEY(`id`)
+	CONSTRAINT `hardwareitemsets_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `maintenanceremarks` (
-	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
-	`createdAt` timestamp NOT NULL DEFAULT (now()),
-	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-	`description` mediumtext NOT NULL,
-	`resolution` enum('resolved','unresolved','n_a') NOT NULL DEFAULT 'n_a',
-	`resolutionMinutes` int NOT NULL DEFAULT 0,
-	`submittedBy` varchar(64) NOT NULL DEFAULT '',
-	`maintenanceTicketId` bigint unsigned NOT NULL,
-	CONSTRAINT `maintenanceremarks_id` PRIMARY KEY(`id`)
-);
---> statement-breakpoint
-CREATE TABLE `maintenanceschedules` (
+CREATE TABLE `hardwareschedules` (
 	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
 	`createdAt` timestamp NOT NULL DEFAULT (now()),
 	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
@@ -60,36 +39,56 @@ CREATE TABLE `maintenanceschedules` (
 	`months` int NOT NULL DEFAULT 0,
 	`weeks` int NOT NULL DEFAULT 0,
 	`days` int NOT NULL DEFAULT 0,
-	`maintenanceItemId` bigint unsigned NOT NULL,
-	`maintenanceTaskId` bigint unsigned NOT NULL,
+	`hardwareItemId` bigint unsigned NOT NULL,
+	`hardwareTaskId` bigint unsigned NOT NULL,
 	`description` mediumtext NOT NULL,
 	`jobsId` varchar(255) NOT NULL DEFAULT '',
-	CONSTRAINT `maintenanceschedules_id` PRIMARY KEY(`id`)
+	CONSTRAINT `hardwareschedules_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `maintenancetasks` (
+CREATE TABLE `hardwaretasks` (
 	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
 	`createdAt` timestamp NOT NULL DEFAULT (now()),
 	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
 	`description` mediumtext NOT NULL,
-	`maintenanceItemSetId` bigint unsigned NOT NULL,
-	CONSTRAINT `maintenancetasks_id` PRIMARY KEY(`id`)
+	`hardwareItemSetId` bigint unsigned NOT NULL,
+	CONSTRAINT `hardwaretasks_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `maintenancetickets` (
+CREATE TABLE `hardwaretickets` (
 	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
 	`createdAt` timestamp NOT NULL DEFAULT (now()),
 	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-	`ticketNumber` int NOT NULL DEFAULT 0,
 	`description` mediumtext NOT NULL,
 	`type` enum('routine','needs_help') NOT NULL DEFAULT 'routine',
 	`severity` enum('working','intermittent','down') NOT NULL DEFAULT 'working',
 	`status` enum('resolved','unresolved') NOT NULL DEFAULT 'unresolved',
 	`resolutionMinutes` int NOT NULL DEFAULT 0,
 	`submittedBy` varchar(64) NOT NULL DEFAULT '',
-	`maintenanceTaskId` bigint unsigned NOT NULL,
-	`maintenanceItemId` bigint unsigned NOT NULL,
-	CONSTRAINT `maintenancetickets_id` PRIMARY KEY(`id`)
+	`hardwareTaskId` bigint unsigned NOT NULL,
+	`hardwareItemId` bigint unsigned NOT NULL,
+	CONSTRAINT `hardwaretickets_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `hardwareticketfiles` (
+	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`s3FileId` bigint unsigned NOT NULL,
+	`hardwareTicketId` bigint unsigned NOT NULL,
+	CONSTRAINT `hardwareticketfiles_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `hardwareticketremarks` (
+	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
+	`createdAt` timestamp NOT NULL DEFAULT (now()),
+	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
+	`description` mediumtext NOT NULL,
+	`resolution` enum('resolved','unresolved','n_a') NOT NULL DEFAULT 'n_a',
+	`resolutionMinutes` int NOT NULL DEFAULT 0,
+	`submittedBy` varchar(64) NOT NULL DEFAULT '',
+	`hardwareTicketId` bigint unsigned NOT NULL,
+	CONSTRAINT `hardwareticketremarks_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
 CREATE TABLE `s3files` (
@@ -126,20 +125,22 @@ CREATE TABLE `users` (
 	`avatarType` enum('robot','uploaded') NOT NULL DEFAULT 'robot',
 	`bio` mediumtext NOT NULL,
 	`avatarId` bigint unsigned DEFAULT null,
+	`tgcUserId` char(36) DEFAULT null,
 	CONSTRAINT `users_id` PRIMARY KEY(`id`),
 	CONSTRAINT `usernameIndex` UNIQUE(`username`),
-	CONSTRAINT `emailIndex` UNIQUE(`email`)
+	CONSTRAINT `emailIndex` UNIQUE(`email`),
+	CONSTRAINT `tgcUserIdIndex` UNIQUE(`tgcUserId`)
 );
 --> statement-breakpoint
 ALTER TABLE `apikeys` ADD CONSTRAINT `apikeys_user_90ada4_fk` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenancefiles` ADD CONSTRAINT `maintenancefiles_s3file_bc83f09_fk` FOREIGN KEY (`s3FileId`) REFERENCES `s3files`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenancefiles` ADD CONSTRAINT `maintenancefiles_ticket_71bc799_fk` FOREIGN KEY (`maintenanceTicketId`) REFERENCES `maintenancetickets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenanceitems` ADD CONSTRAINT `maintenanceitems_itemSet_5f29c163_fk` FOREIGN KEY (`maintenanceItemSetId`) REFERENCES `maintenanceitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenanceremarks` ADD CONSTRAINT `maintenanceremarks_ticket_7bde9655_fk` FOREIGN KEY (`maintenanceTicketId`) REFERENCES `maintenancetickets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenanceschedules` ADD CONSTRAINT `maintenanceschedules_item_7745e177_fk` FOREIGN KEY (`maintenanceItemId`) REFERENCES `maintenanceitems`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenanceschedules` ADD CONSTRAINT `maintenanceschedules_task_77412705_fk` FOREIGN KEY (`maintenanceTaskId`) REFERENCES `maintenancetasks`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenancetasks` ADD CONSTRAINT `maintenancetasks_itemSet_75c8db95_fk` FOREIGN KEY (`maintenanceItemSetId`) REFERENCES `maintenanceitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenancetickets` ADD CONSTRAINT `maintenancetickets_task_1206e190_fk` FOREIGN KEY (`maintenanceTaskId`) REFERENCES `maintenancetasks`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE `maintenancetickets` ADD CONSTRAINT `maintenancetickets_item_120b9c02_fk` FOREIGN KEY (`maintenanceItemId`) REFERENCES `maintenanceitems`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwareitems` ADD CONSTRAINT `hardwareitems_itemSet_fef4de8_fk` FOREIGN KEY (`hardwareItemSetId`) REFERENCES `hardwareitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwareschedules` ADD CONSTRAINT `hardwareschedules_item_33d0762_fk` FOREIGN KEY (`hardwareItemId`) REFERENCES `hardwareitems`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwareschedules` ADD CONSTRAINT `hardwareschedules_task_3384cf0_fk` FOREIGN KEY (`hardwareTaskId`) REFERENCES `hardwaretasks`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwaretasks` ADD CONSTRAINT `hardwaretasks_itemSet_6afcc4a_fk` FOREIGN KEY (`hardwareItemSetId`) REFERENCES `hardwareitemsets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwaretickets` ADD CONSTRAINT `hardwaretickets_task_652b79c5_fk` FOREIGN KEY (`hardwareTaskId`) REFERENCES `hardwaretasks`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwaretickets` ADD CONSTRAINT `hardwaretickets_item_6526bf53_fk` FOREIGN KEY (`hardwareItemId`) REFERENCES `hardwareitems`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwareticketfiles` ADD CONSTRAINT `hardwareticketfiles_s3file_77c6b8a8_fk` FOREIGN KEY (`s3FileId`) REFERENCES `s3files`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwareticketfiles` ADD CONSTRAINT `hardwareticketfiles_ticket_731a4138_fk` FOREIGN KEY (`hardwareTicketId`) REFERENCES `hardwaretickets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
+ALTER TABLE `hardwareticketremarks` ADD CONSTRAINT `hardwareticketremarks_ticket_1ddadbcc_fk` FOREIGN KEY (`hardwareTicketId`) REFERENCES `hardwaretickets`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
 ALTER TABLE `s3files` ADD CONSTRAINT `s3files_user_40cb3d4d_fk` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
 ALTER TABLE `users` ADD CONSTRAINT `users_avatar_39d62890_fk` FOREIGN KEY (`avatarId`) REFERENCES `s3files`(`id`) ON DELETE set null ON UPDATE no action;
--- ving/drizzle/migrations/0001_great_the_executioner.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `users` ADD `tgcUserId` char(36) DEFAULT null;
