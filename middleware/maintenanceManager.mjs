2631e5c6bce63d8df9660dcb211c9a681fc0bf11
colin <colink@perldreamer.com>
2024-06-24 09:31:01 -0700

    Remove file that doesn't belong here.

--- middleware/maintenanceManager.mjs
+++
@@ -1,7 +0,0 @@
-import { ouch } from "#ving/utils/ouch.mjs";
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const currentUser = useCurrentUserStore();
-  if (!currentUser.props?.maintenanceManager) {
-    throw ouch(403, "You must be a maintenance manager to view this.");
-  }
-});
