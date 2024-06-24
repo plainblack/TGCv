578de465d05c554670c8f5c01055aa250bfd8748
colin <colink@perldreamer.com>
2024-06-24 15:26:59 -0700

    Delete hardware the right way

--- import_tgc_hardware.mjs
+++ import_tgc_hardware.mjs
@@ -7,35 +7,39 @@ const Sets = await useKind('HardwareItemSet');
 const Items = await useKind('HardwareItem');
 const Tasks = await useKind('HardwareTask');
 
-await Sets.delete;
-await Items.delete;
-await Tasks.delete;
+await Sets.deleteMany();
+await Items.deleteMany();
+await Tasks.deleteMany();
 
-const seenSets = [];
+if (1) {
 
-for (const item of maintenanceItems) {
-    //Set == station
-    if (!seenSets[item.station]) {
-        //Create the Set
-        const set = await Sets.create({
-            name: item.station,
-            status: item.status == 'In Use' ? 'in_use' : 'retired',
-        });
-        seenSets[item.station] = set;
-        //Create tasks for the Set
-        for (const task of item.tasks) {
-            await Tasks.create({
-                description: task,
-                hardwareItemSetId: set.id,
+    const seenSets = [];
+
+    for (const item of maintenanceItems) {
+        //Set == station
+        if (!seenSets[item.station]) {
+            //Create the Set
+            const set = await Sets.create({
+                name: item.station,
+                status: item.status == 'In Use' ? 'in_use' : 'retired',
             });
+            seenSets[item.station] = set;
+            //Create tasks for the Set
+            for (const task of item.tasks) {
+                await Tasks.create({
+                    description: task,
+                    hardwareItemSetId: set.id,
+                });
+            }
         }
+        //Create the item
+        await Items.create({
+            name: item.item,
+            status: item.status == 'In Use' ? 'in_use' : 'retired',
+            hardwareItemSetId: seenSets[item.station].id,
+        });
     }
-    //Create the item
-    await Items.create({
-        name: item.item,
-        status: item.status == 'In Use' ? 'in_use' : 'retired',
-        hardwareItemSetId: seenSets[item.station].id,
-    });
+
 }
 
 console.log("Done");
