19d5535f4dbb54e63a5c61336a7ee4d7ca4b268a
colin <colink@perldreamer.com>
2024-07-01 07:33:24 -0700

    Formatting

--- maintenanceSchedules.mjs
+++ maintenanceSchedules.mjs
@@ -1,1 +1,4 @@
-export const maintenanceSchedules = [{"months":0,"days":2,"event":"Clean Blade","note":"","recurrence":"weekly","weeks":0,"equipment":"Cutting Plotter A (Graphtec FC2250)"},{"note":"","equipment":"Cutting Plotter A (Graphtec FC2250)","weeks":0,"recurrence":"daily","days":0,"months":0,"event":"Clean Blade"}];
+export const maintenanceSchedules = [
+    { "months": 0, "days": 2, "event": "Clean Blade", "note": "", "recurrence": "weekly", "weeks": 0, "equipment": "Cutting Plotter A (Graphtec FC2250)" },
+    { "note": "", "equipment": "Cutting Plotter A (Graphtec FC2250)", "weeks": 0, "recurrence": "daily", "days": 0, "months": 0, "event": "Clean Blade" }
+];
