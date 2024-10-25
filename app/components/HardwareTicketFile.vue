86efeb71290a85f544e22b430b017c269bf19954
JT Smith <jt@plainblack.com>
2024-10-25 17:37:00 -0500

    fix the width of files

--- app/components/HardwareTicketFile.vue
+++ app/components/HardwareTicketFile.vue
@@ -3,8 +3,8 @@
      <Chip class="mr-1" v-tooltip="mFile.related?.s3file?.props?.sizeInBytes + ' bytes'" >
    
         <a :href="mFile.related?.s3file?.meta?.fileUrl" class="inline-block">
-            <span class="vertical-align-middle"><img :src="mFile.related?.s3file?.meta?.thumbnailUrl"></span>
-            <span class="vertical-align-middle">{{ mFile.related?.s3file?.props?.filename }}</span>
+            <span class="vertical-align-middle"><img :src="mFile.related?.s3file?.meta?.thumbnailUrl" class="max-w-40"></span>
+            <span class="vertical-align-middle"><span class="inline-block max-w-40 overflow-hidden">{{ mFile.related?.s3file?.props?.filename }}</span></span>
         </a>
          <Button v-if="mFile.meta?.isOwner"  title="Delete" alt="Delete Maintenance File" icon="pi pi-trash" text severity="danger" @click="mFile.delete()" />
     </Chip>
