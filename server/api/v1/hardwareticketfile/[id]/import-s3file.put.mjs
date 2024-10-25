107b21e638975a6c8f9f4bbea9764ceb4695ac47
JT Smith <jt@plainblack.com>
2024-10-25 15:47:09 -0500

    remove things that aren't needed for this uploader

--- server/api/v1/hardwareticketfile/[id]/import-s3file.put.mjs
+++ server/api/v1/hardwareticketfile/[id]/import-s3file.put.mjs
@@ -6,9 +6,6 @@ export default defineEventHandler(async (event) => {
     const hardwareticketfiles = await useKind('HardwareTicketFile');
     const { id } = getRouterParams(event);
     const hardwareticketfile = await hardwareticketfiles.findOrDie(id);
-    let oldFile = undefined;
-    if (hardwareticketfile.get('s3FileId'))
-        oldFile = await hardwareticketfile.parent('s3file');
     const session = obtainSession(event);
     await hardwareticketfile.canEdit(session);
     const body = await getBody(event);
@@ -16,10 +13,7 @@ export default defineEventHandler(async (event) => {
     const s3file = await s3files.findOrDie(body.s3FileId);
     await s3file.postProcessFile();
     await s3file.verifyExtension(hardwareticketfile.parentPropSchema('s3file').relation.acceptedFileExtensions);
-    await s3file.verifyExactDimensions(300, 300);
     hardwareticketfile.set('s3FileId', s3file.get('id'));
     await hardwareticketfile.update();
-    if (oldFile)
-        await oldFile.delete();
     return await hardwareticketfile.describe(describeParams(event, session));
 });
