d234c7f5df8bf353fbec2dd7f076f45cf3b12842
JT Smith <jt@plainblack.com>
2024-10-25 17:37:28 -0500

    better layout

--- app/pages/hardwareticket/[id]/index.vue
+++ app/pages/hardwareticket/[id]/index.vue
@@ -5,15 +5,19 @@
             <PanelNav :links="links" />
         </template>
         <template #content>
-            <MarkdownView :text="hardwareticket.props?.description" class="px-2" />
 
-            <div class="mt-3" v-if="hardwareticket.meta?.isOwner">
+            <FieldsetNav v-if="hardwareticket.props">
+                <FieldsetItem name="Description">
+                    <MarkdownView :text="hardwareticket.props?.description" class="px-2" />
+                </FieldsetItem>
 
-                <Button @click="showAddComment = !showAddComment" severity="success" title="Add Comment" class=" mr-2 mb-2"
-                alt="Comment on Ticket"><i class="pi pi-comment mr-1"></i> Add Comment / Resolve</Button>
+                <FieldsetItem name="Actions" v-if="hardwareticket.meta?.isOwner">
 
-                <Button @click="showAddFiles = !showAddFiles" severity="primary" title="Add Files" class=" mr-2 mb-2"
-                alt="Upload Files to Ticket"><i class="pi pi-file mr-1"></i> Add Files</Button>
+                    <Button @click="showAddComment = !showAddComment" severity="success" title="Add Comment" class=" mr-2 mb-2"
+                    alt="Comment on Ticket"><i class="pi pi-comment mr-1"></i> Add Comment / Resolve</Button>
+
+                    <Button @click="showAddFiles = !showAddFiles" severity="primary" title="Add Files" class=" mr-2 mb-2"
+                    alt="Upload Files to Ticket"><i class="pi pi-file mr-1"></i> Add Files</Button>
 
 
                     <NuxtLink :to="`/hardwareticket/${hardwareticket.props?.id}/edit`" class="no-underline mr-2 mb-2">
@@ -23,89 +27,91 @@
                     </NuxtLink>
                     <Button @click="hardwareticket.delete()" severity="danger" title="Delete" class=" mr-2 mb-2"
                         alt="Delete Hardware Ticket"><i class="pi pi-trash mr-1"></i> Delete</Button>
-                </div>
+                </FieldsetItem>
 
 
-            <PanelZone v-if="showAddFiles" title="Add Files">
-                <client-only>
-                    <Dropzone :afterUpload="uploadHardwareFile">
-                    </Dropzone>
-                </client-only>
-            </PanelZone>
+                <FieldsetItem v-if="showAddFiles" name="Add Files">
+                    <client-only>
+                        <Dropzone :afterUpload="uploadHardwareFile" :acceptedFiles="['png', 'jpeg', 'jpg', 'gif', 'pdf', 'tiff', 'svg', 'dxf', 'csv', 'zip']">
+                        </Dropzone>
+                    </client-only>
+                </FieldsetItem>
 
-            <PanelZone v-if="showAddComment" title="Add Comment">
-                <Form :send="() => hardwareremarks.create()">
-                    <div class="grid">
+                <FieldsetItem v-if="showAddComment" name="Add Comment">
+                    <Form :send="() => hardwareremarks.create()">
+                        <div class="grid">
 
-                        <div class="col-8">
-                            <div class="mb-4">
-                                <FormInput name="description" type="markdown"
-                                    v-model="hardwareremarks.new.description" required label="Comment" />
+                            <div class="col-8">
+                                <div class="mb-4">
+                                    <FormInput name="description" type="markdown"
+                                        v-model="hardwareremarks.new.description" required label="Comment" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-4">
+                                    <FormInput type="select" name="resolution"
+                                        :options="hardwareremarks.propsOptions?.resolution"
+                                        v-model="hardwareremarks.new.resolution" label="Resolution" />
+                                </div>
+                                <div class="mb-4">
+                                    <FormInput name="submittedBy" type="text" v-model="hardwareremarks.new.submittedBy"
+                                        required label="Submitted By" />
+                                </div>
+                                <div class="mb-4">
+                                    <FormInput name="resolutionMinutes" type="number" append="minutes"
+                                        v-model="hardwareremarks.new.resolutionMinutes" label="Time Spent" />
+                                </div>
+                                <div>
+                                    <Button type="submit" class="w-auto" severity="success">
+                                        <i class="pi pi-plus mr-1"></i> Add Comment
+                                    </Button>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="mb-4">
-                                <FormInput type="select" name="resolution"
-                                    :options="hardwareremarks.propsOptions?.resolution"
-                                    v-model="hardwareremarks.new.resolution" label="Resolution" />
-                            </div>
-                            <div class="mb-4">
-                                <FormInput name="submittedBy" type="text" v-model="hardwareremarks.new.submittedBy"
-                                    required label="Submitted By" />
-                            </div>
-                            <div class="mb-4">
-                                <FormInput name="resolutionMinutes" type="number" append="minutes"
-                                    v-model="hardwareremarks.new.resolutionMinutes" label="Time Spent" />
-                            </div>
-                            <div>
-                                <Button type="submit" class="w-auto" severity="success">
-                                    <i class="pi pi-plus mr-1"></i> Add Comment
-                                </Button>
-                            </div>
-                        </div>
+                    </Form>
+                </FieldsetItem>
+
+                <FieldsetItem name="Files" v-if="hardwarefiles.records.length > 0">
+                    <HardwareTicketFile v-for=" file  in  hardwarefiles.records " :key="file.props.id" :mFile="file" />
+                </FieldsetItem>
+
+                <FieldsetItem name="Comments" v-if="hardwareremarks.records.length > 0">
+
+                    <HardwareTicketRemark v-for=" remark  in  hardwareremarks.records " :key="remark.props?.id"
+                    :remark="remark" />
+                    
+                </FieldsetItem>
+                <FieldsetItem name="Statistics">
+                    <div><b>Item</b>: {{ hardwareticket.related?.item?.props?.name }}
+                        <CopyToClipboard size="xs" :text="hardwareticket.props?.hardwareItemId" />
                     </div>
-                </Form>
-            </PanelZone>
+                    <div><b>Task</b>: {{ hardwareticket.related?.task?.props?.description }}
+                        <CopyToClipboard size="xs" :text="hardwareticket.props?.hardwareTaskId" />
+                    </div>
+                    <div><b>Type</b>: {{ enum2label(hardwareticket.props?.type, hardwareticket.options?.type) }}</div>
 
-            <PanelZone title="Files" v-if="hardwarefiles.records.length > 0">
-                <HardwareTicketFile v-for=" file  in  hardwarefiles.records " :key="file.props.id" :mFile="file" />
-            </PanelZone>
+                    <div><b>Severity</b>: {{ enum2label(hardwareticket.props?.severity,
+            hardwareticket.options?.severity) }}
+                    </div>
 
-            <PanelZone title="Comments" v-if="hardwareremarks.records.length > 0">
+                    <div><b>Status</b>: {{ enum2label(hardwareticket.props?.status, hardwareticket.options?.status) }}
+                    </div>
 
-                <HardwareTicketRemark v-for=" remark  in  hardwareremarks.records " :key="remark.props?.id"
-                :remark="remark" />
-                
-            </PanelZone>
+                    <div><b>Resolution Time</b>: {{ hardwareticket.props?.resolutionMinutes }} minutes</div>
+
+                    <div><b>Submitted By</b>: {{ hardwareticket.props?.submittedBy }}</div>
+
+                    <div><b>Id</b>: {{ hardwareticket.props?.id }}
+                        <CopyToClipboard size="xs" :text="hardwareticket.props?.id" />
+                    </div>
+
+                    <div><b>Created At</b>: {{ formatDateTime(hardwareticket.props?.createdAt) }}</div>
+
+                    <div><b>Updated At</b>: {{ formatDateTime(hardwareticket.props?.updatedAt) }}</div>
+                </FieldsetItem>
+            </FieldsetNav>
         </template>
-        <template #right>
-            <div><b>Item</b>: {{ hardwareticket.related?.item?.props?.name }}
-                    <CopyToClipboard size="xs" :text="hardwareticket.props?.hardwareItemId" />
-                </div>
-                <div><b>Task</b>: {{ hardwareticket.related?.task?.props?.description }}
-                    <CopyToClipboard size="xs" :text="hardwareticket.props?.hardwareTaskId" />
-                </div>
-                <div><b>Type</b>: {{ enum2label(hardwareticket.props?.type, hardwareticket.options?.type) }}</div>
-
-                <div><b>Severity</b>: {{ enum2label(hardwareticket.props?.severity,
-        hardwareticket.options?.severity) }}
-                </div>
-
-                <div><b>Status</b>: {{ enum2label(hardwareticket.props?.status, hardwareticket.options?.status) }}
-                </div>
-
-                <div><b>Resolution Time</b>: {{ hardwareticket.props?.resolutionMinutes }} minutes</div>
-
-                <div><b>Submitted By</b>: {{ hardwareticket.props?.submittedBy }}</div>
-
-                <div><b>Id</b>: {{ hardwareticket.props?.id }}
-                    <CopyToClipboard size="xs" :text="hardwareticket.props?.id" />
-                </div>
-
-                <div><b>Created At</b>: {{ formatDateTime(hardwareticket.props?.createdAt) }}</div>
-
-                <div><b>Updated At</b>: {{ formatDateTime(hardwareticket.props?.updatedAt) }}</div>
-        </template>
+        
     </PanelFrame>
 </template>
 
