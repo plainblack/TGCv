7a84d3523dac52fc8cfe5f0fb4c396fa532f5b6f
JT Smith <jt@plainblack.com>
2024-10-25 14:35:37 -0500

    fixed formatting of the ticket page is bad #14

--- app/pages/hardwareticket/[id]/index.vue
+++ app/pages/hardwareticket/[id]/index.vue
@@ -5,18 +5,85 @@
             <PanelNav :links="links" />
         </template>
         <template #content>
-            <PanelZone :title="`Ticket ${hardwareticket.meta?.ticketNumber}`">
+            <MarkdownView :text="hardwareticket.props?.description" class="px-2" />
 
-                <div><b>Item</b>: {{ hardwareticket.related?.item?.props?.name }}
+            <div class="mt-3" v-if="hardwareticket.meta?.isOwner">
+
+                <Button @click="showAddComment = !showAddComment" severity="success" title="Add Comment" class=" mr-2 mb-2"
+                alt="Comment on Ticket"><i class="pi pi-comment mr-1"></i> Add Comment / Resolve</Button>
+
+                <Button @click="showAddFiles = !showAddFiles" severity="primary" title="Add Files" class=" mr-2 mb-2"
+                alt="Upload Files to Ticket"><i class="pi pi-file mr-1"></i> Add Files</Button>
+
+
+                    <NuxtLink :to="`/hardwareticket/${hardwareticket.props?.id}/edit`" class="no-underline mr-2 mb-2">
+                        <Button severity="primary" title="Edit" alt="Edit Hardware Ticket"><i
+                                class="pi pi-pencil mr-1"></i>
+                            Edit</Button>
+                    </NuxtLink>
+                    <Button @click="hardwareticket.delete()" severity="danger" title="Delete" class=" mr-2 mb-2"
+                        alt="Delete Hardware Ticket"><i class="pi pi-trash mr-1"></i> Delete</Button>
+                </div>
+
+            <PanelZone title="Files" v-if="hardwarefiles.records.length > 0">
+                <HardwareTicketFile v-for=" file  in  hardwarefiles.records " :key="file.props.id" :mFile="file" />
+            </PanelZone>
+
+            <PanelZone v-if="showAddFiles" title="Add Files">
+                <client-only>
+                    <Dropzone :afterUpload="uploadHardwareFile">
+                    </Dropzone>
+                </client-only>
+            </PanelZone>
+
+            <PanelZone v-if="showAddComment" title="Add Comment">
+                <Form :send="() => hardwareremarks.create()">
+                    <div class="grid">
+
+                        <div class="col-8">
+                            <div class="mb-4">
+                                <FormInput name="description" type="markdown"
+                                    v-model="hardwareremarks.new.description" required label="Comment" />
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="mb-4">
+                                <FormInput type="select" name="resolution"
+                                    :options="hardwareremarks.propsOptions?.resolution"
+                                    v-model="hardwareremarks.new.resolution" label="Resolution" />
+                            </div>
+                            <div class="mb-4">
+                                <FormInput name="submittedBy" type="text" v-model="hardwareremarks.new.submittedBy"
+                                    required label="Submitted By" />
+                            </div>
+                            <div class="mb-4">
+                                <FormInput name="resolutionMinutes" type="number" append="minutes"
+                                    v-model="hardwareremarks.new.resolutionMinutes" label="Time Spent" />
+                            </div>
+                            <div>
+                                <Button type="submit" class="w-auto" severity="success">
+                                    <i class="pi pi-plus mr-1"></i> Add Comment
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </Form>
+            </PanelZone>
+
+            <PanelZone title="Comments" v-if="hardwareremarks.records.length > 0">
+
+                <HardwareTicketRemark v-for=" remark  in  hardwareremarks.records " :key="remark.props?.id"
+                :remark="remark" />
+                
+            </PanelZone>
+        </template>
+        <template #right>
+            <div><b>Item</b>: {{ hardwareticket.related?.item?.props?.name }}
                     <CopyToClipboard size="xs" :text="hardwareticket.props?.hardwareItemId" />
                 </div>
                 <div><b>Task</b>: {{ hardwareticket.related?.task?.props?.description }}
                     <CopyToClipboard size="xs" :text="hardwareticket.props?.hardwareTaskId" />
                 </div>
-                <div><b>Description</b>:
-                    <MarkdownView :text="hardwareticket.props?.description" />
-                </div>
-
                 <div><b>Type</b>: {{ enum2label(hardwareticket.props?.type, hardwareticket.options?.type) }}</div>
 
                 <div><b>Severity</b>: {{ enum2label(hardwareticket.props?.severity,
@@ -26,7 +93,7 @@
                 <div><b>Status</b>: {{ enum2label(hardwareticket.props?.status, hardwareticket.options?.status) }}
                 </div>
 
-                <div><b>Resolution Minutes</b>: {{ hardwareticket.props?.resolutionMinutes }}</div>
+                <div><b>Resolution Time</b>: {{ hardwareticket.props?.resolutionMinutes }} minutes</div>
 
                 <div><b>Submitted By</b>: {{ hardwareticket.props?.submittedBy }}</div>
 
@@ -37,67 +104,6 @@
                 <div><b>Created At</b>: {{ formatDateTime(hardwareticket.props?.createdAt) }}</div>
 
                 <div><b>Updated At</b>: {{ formatDateTime(hardwareticket.props?.updatedAt) }}</div>
-
-                <HardwareTicketFile v-for=" file  in  hardwarefiles.records " :key="file.props.id" :mFile="file" />
-                <div class="mt-5 surface-card p-5 border-1 surface-border border-round">
-                    <h2 class="mt-0">Add Comment</h2>
-
-                    <Form :send="() => hardwareremarks.create()">
-                        <div class="grid">
-
-                            <div class="col-8">
-                                <div class="mb-4">
-                                    <FormInput name="description" type="markdown"
-                                        v-model="hardwareremarks.new.description" required label="Description" />
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="mb-4">
-                                    <FormInput type="select" name="resolution"
-                                        :options="hardwareremarks.propsOptions?.resolution"
-                                        v-model="hardwareremarks.new.resolution" label="Resolution" />
-                                </div>
-                                <div class="mb-4">
-                                    <FormInput name="submittedBy" type="text" v-model="hardwareremarks.new.submittedBy"
-                                        required label="Submitted By" />
-                                </div>
-                                <div class="mb-4">
-                                    <FormInput name="resolutionMinutes" type="number"
-                                        v-model="hardwareremarks.new.resolutionMinutes" label="Time Spent" />
-                                </div>
-                                <div>
-                                    <Button type="submit" class="w-auto" severity="success">
-                                        <i class="pi pi-plus mr-1"></i> Add Comment
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-                    </Form>
-                </div>
-
-                <HardwareTicketRemark v-for=" remark  in  hardwareremarks.records " :key="remark.props?.id"
-                    :remark="remark" />
-
-                <div class="mt-5 surface-card p-5 border-1 surface-border border-round">
-                    <h2 class="mt-0">Add Files</h2>
-                    <div class="mb-4">
-                        <client-only>
-                            <Dropzone :afterUpload="uploadHardwareFile">
-                            </Dropzone>
-                        </client-only>
-                    </div>
-                </div>
-
-                <div class="mt-3" v-if="hardwareticket.meta?.isOwner">
-                    <NuxtLink :to="`/hardwareticket/${hardwareticket.props?.id}/edit`" class="no-underline mr-2 mb-2">
-                        <Button severity="success" title="Edit" alt="Edit Hardware Ticket"><i
-                                class="pi pi-pencil mr-1"></i>
-                            Edit</Button>
-                    </NuxtLink>
-                    <Button @click="hardwareticket.delete()" severity="danger" title="Delete"
-                        alt="Delete Hardware Ticket"><i class="pi pi-trash mr-1"></i> Delete</Button>
-                </div>
-            </PanelZone>
         </template>
     </PanelFrame>
 </template>
@@ -109,6 +115,9 @@ definePageMeta({
 const route = useRoute();
 const id = route.params.id.toString();
 const links = useHardwareLinks();
+
+let showAddFiles = ref(false);
+let showAddComment = ref(false);
 
 const hardwareticket = useVingRecord({
     id,
@@ -126,6 +135,7 @@ const hardwareremarks = useVingKind({
     query: { includeMeta: true, sortBy: 'createdAt', sortOrder: 'desc', },
     newDefaults: { description: '', resolution: 'n_a', resolutionMinutes: 0, submittedBy: 'YOUR INITIALS', hardwareTicketId: hardwareticket.props?.id },
     async onCreate() {
+        showAddComment.value = false;
         await hardwareticket.fetch();
     },
     async onDelete() {
@@ -137,6 +147,9 @@ const hardwarefiles = useVingKind({
     createApi: `/api/${useRestVersion()}/hardwareticketfile`,
     query: { includeMeta: true, sortBy: 'createdAt', sortOrder: 'desc', includeRelated: ['s3file'], },
     newDefaults: { s3FileId: '', hardwareTicketId: hardwareticket.props?.id },
+    async onCreate() {
+        showAddFiles.value = false;
+    },
 });
 
 const uploadHardwareFile = async (s3file) => {
