49e1cf62c548f3ba15a3fb3cd69efec74a2c59e9
JT Smith <jt@plainblack.com>
2024-10-25 13:03:25 -0500

    if the user tries to go home, redirect to hardware tickets

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,6 @@
 <template>
-    <Title>{{ config.public?.site?.name }}</Title>
-    homepage goes here
-    <Button @mousedown="doit()">Doit</Button>
+    
 </template>
-
 <script setup>
-const config = useRuntimeConfig();
-
-async function doit() {
-    console.log(await $fetch(`/api/${useRestVersion()}/test?foo=bar`))
-}
+navigateTo('/hardwareticket');
 </script>
