b320ff607093cd074cc6dfd84dc5effb79fbcb1f
colin <colink@perldreamer.com>
2024-10-30 10:58:24 -0700

    Apparently "rename" doesn't to a git move, deleting old auth plugins.

--- app/middleware/maintenanceProductionManager.mjs
+++
@@ -1,7 +0,0 @@
-import { ouch } from "#ving/utils/ouch.mjs";
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const currentUser = useCurrentUserStore();
-  if (!currentUser.props?.maintenanceManager && !currentUser.props?.productionManager) {
-    throw ouch(403, "You must be a maintenance or production manager to view this.");
-  }
-});
