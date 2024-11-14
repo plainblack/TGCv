1505647d34b7e810f6f510e6cbb9e71d9b5c720e
colin <colink@perldreamer.com>
2024-11-14 13:15:00 -0800

    Let admin see pages via middleware.  Interim fix.

--- app/middleware/maintenance-manager.mjs
+++ app/middleware/maintenance-manager.mjs
@@ -1,7 +1,7 @@
 import { ouch } from "#ving/utils/ouch.mjs";
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const currentUser = useCurrentUser();
-  if (!currentUser.props?.maintenanceManager) {
+  if (!currentUser.props?.maintenanceManager && !currentUser.props?.admin) {
     return abortNavigation({ statusCode: 403, fatal: true, message: 'You must be a maintenance manager to view this.' });
   }
 });
--- app/middleware/maintenance-production-manager.mjs
+++ app/middleware/maintenance-production-manager.mjs
@@ -1,7 +1,7 @@
 import { ouch } from "#ving/utils/ouch.mjs";
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const currentUser = useCurrentUser();
-  if (!currentUser.props?.maintenanceManager && !currentUser.props?.productionManager) {
+  if (!currentUser.props?.maintenanceManager && !currentUser.props?.productionManager && !currentUser.props?.admin) {
     return abortNavigation({ statusCode: 403, fatal: true, message: 'You must be a maintenance or production manager to view this.' });
   }
 });
