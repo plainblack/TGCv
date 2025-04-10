db11e54e36e56f75457019df5ed37fa77d668942
colin <colink@perldreamer.com>
2025-04-10 16:52:50 -0700

    Update nuxt config format/date compatibility

--- nuxt.config.mjs
+++ nuxt.config.mjs
@@ -6,79 +6,90 @@ import path from 'node:path';
 ving.site.url = process.env.VING_SITE_URL;
 
 export default defineNuxtConfig({
-    devtools: false,
-    future: {
-        compatibilityVersion: 4,
-    },
-    modules: [
-        '@pinia/nuxt',
-        'nuxt-icon',
-        '@nuxtjs/tailwindcss',
-        '@primevue/nuxt-module',
-        '@vueuse/nuxt',
-    ],
-    primevue: {
-        importTheme: { from: "@/themes/ving-theme.mjs" },
-    },
-    imports: {
-        dirs: [
-            'composables/**',
-            'utils/**',
-        ],
-    },
-    components: [
-        {
-            path: '~/components',
-            pathPrefix: false,
-        },
-    ],
-    app: {
-        head: {
-            link: [
-                {
-                    rel: 'apple-touch-icon',
-                    href: '/apple-touch-icon.png',
-                    sizes: '180x180'
-                },
-                {
-                    rel: 'icon',
-                    type: 'image/png',
-                    sizes: '32x32',
-                    href: '/favicon-32x32.png',
-                },
-                {
-                    rel: 'icon',
-                    type: 'image/png',
-                    sizes: '16x16',
-                    href: '/favicon-16x16.png',
-                },
-                {
-                    rel: 'manifest',
-                    href: '/manifest.json',
-                },
-            ],
-            meta: [
-                {
-                    name: 'msapplication-TileColor',
-                    content: "#da532c",
-                },
-                {
-                    name: "theme-color",
-                    content: "#ffffff",
-                },
-            ]
-        },
-    },
-    css: [
-        'primeicons/primeicons.css',
-    ],
-    runtimeConfig: {
-        public: ving,
-    },
-    hooks: {
-        'nitro:build:public-assets': (nitro) => {
-            const targetPath = path.join(nitro.options.output.dir, 'ving.json');
-            cpSync('./ving.json', targetPath, { recursive: true });
-        }
-    }
+  devtools: false,
+
+  future: {
+      compatibilityVersion: 4,
+  },
+
+  modules: [
+      '@pinia/nuxt',
+      'nuxt-icon',
+      '@nuxtjs/tailwindcss',
+      '@primevue/nuxt-module',
+      '@vueuse/nuxt',
+  ],
+
+  primevue: {
+      importTheme: { from: "@/themes/ving-theme.mjs" },
+  },
+
+  imports: {
+      dirs: [
+          'composables/**',
+          'utils/**',
+      ],
+  },
+
+  components: [
+      {
+          path: '~/components',
+          pathPrefix: false,
+      },
+  ],
+
+  app: {
+      head: {
+          link: [
+              {
+                  rel: 'apple-touch-icon',
+                  href: '/apple-touch-icon.png',
+                  sizes: '180x180'
+              },
+              {
+                  rel: 'icon',
+                  type: 'image/png',
+                  sizes: '32x32',
+                  href: '/favicon-32x32.png',
+              },
+              {
+                  rel: 'icon',
+                  type: 'image/png',
+                  sizes: '16x16',
+                  href: '/favicon-16x16.png',
+              },
+              {
+                  rel: 'manifest',
+                  href: '/manifest.json',
+              },
+          ],
+          meta: [
+              {
+                  name: 'msapplication-TileColor',
+                  content: "#da532c",
+              },
+              {
+                  name: "theme-color",
+                  content: "#ffffff",
+              },
+          ]
+      },
+  },
+
+  css: [
+      'primeicons/primeicons.css',
+  ],
+
+  runtimeConfig: {
+      public: ving,
+  },
+
+  hooks: {
+      'nitro:build:public-assets': (nitro) => {
+          const targetPath = path.join(nitro.options.output.dir, 'ving.json');
+          cpSync('./ving.json', targetPath, { recursive: true });
+      }
+  },
+
+  compatibilityDate: '2025-04-10'
 })
