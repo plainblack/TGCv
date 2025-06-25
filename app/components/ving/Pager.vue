06f2a8afcf5149c8da21e2d191d5734d9aa8d07c
colin <colink@perldreamer.com>
2025-06-25 10:06:11 -0700

    Add rows per page UI and callback to the Pager

--- app/components/ving/Pager.vue
+++ app/components/ving/Pager.vue
@@ -2,11 +2,11 @@
     <Paginator :template="{
             '640px': 'PrevPageLink CurrentPageReport NextPageLink',
             '960px': 'FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink',
-            '1300px': 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink',
-            default: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink JumpToPageDropdown'
+            '1300px': 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown',
+            default: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink JumpToPageDropdown RowsPerPageDropdown'
         }"
         :rowsPerPageOptions="[10, 20, 50, 100]" :alwaysShow="false" :totalRecords="kind.paging.totalItems"
-        :rows="kind.paging.itemsPerPage" :pageCount="kind.paging.totalPages" :page="kind.paging.page" @page="changePage">
+        :rows="kind.paging.itemsPerPage" :pageCount="kind.paging.totalPages" :page="kind.paging.page" @page="changePage" @update:rows="changePageSize">
     </Paginator>
 </template>
 
@@ -15,6 +15,12 @@ const props = defineProps(['kind']);
 
 function changePage(pager) {
     props.kind.query.page = pager.page + 1;
+    props.kind.search();
+}
+
+function changePageSize(newSize) {
+    props.kind.query.itemsPerPage = newSize;
+    props.kind.query.page = 1;
     props.kind.search();
 }
 </script>
