9b0650fc6d35aaecc4dfc2d619897ed3ff74ed2f
colin <colink@perldreamer.com>
2025-07-10 10:35:16 -0700

    Fix script to dump URLs

--- fixups/list_hardware_item_ticket_urls.mjs
+++ fixups/list_hardware_item_ticket_urls.mjs
@@ -2,13 +2,12 @@
 
 import { useKind } from '#ving/record/utils.mjs';
 import ving from '#ving/index.mjs';
-import { stringifyId } from '#ving/utils/int2str.mjs';
 
 const Items = await useKind('HardwareItem');
 const allItems = await Items.findAll();
 
 for await (const item of allItems) {
-   console.log('"%s","https://www.thegamecrafter.com/hardwaretickets?hardwareItemId=%s","%s"', item.name, stringifyId(item.id), item.status);
+   console.log('"%s","https://www.thegamecrafter.com/hardwaretickets?hardwareItemId=%s","%s"', item.name, item.idAsString(), item.status);
 }
 
 console.log("Done");
